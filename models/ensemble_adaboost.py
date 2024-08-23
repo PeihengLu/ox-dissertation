0b4caa93b2ced43d050206822a72e8475c92eae5
PHL <s1948149@ed.ac.uk>
2024-08-23 12:17:15 +0100

    trained first version of adaboost

--- models/ensemble_adaboost.py
+++ models/ensemble_adaboost.py
@@ -4,8 +4,10 @@ import torch
 import pickle
 from os.path import join as pjoin, isfile
 import pandas as pd
+import collections
+from scipy.stats import pearsonr, spearmanr
 
-class EmsenbleAdaBoost:
+class EnsembleAdaBoost:
     def __init__(self, n_rounds: int = 10, learning_rate=1.0):
         """ 
         
@@ -13,15 +15,14 @@ class EmsenbleAdaBoost:
         self.n_rounds = n_rounds
         self.learning_rate = learning_rate
         self.base_learners = {
-            'mlp_weighted': mlp_weighted,
-            'ridge_regression': ridge_regression,
-            'random_forest': random_forest,
-            'xgboost': xgboost
+            'xgb': xgboost,
+            'mlp': mlp_weighted,
+            'ridge': ridge_regression,
+            'rf': random_forest,
         }
-        self.dl_models = ['mlp_weighted']
+        self.dl_models = ['mlp']
         self.models = []
         self.alphas = []
-        self.sample_weights = []
         # set the random seed
         np.random.seed(42)
         torch.manual_seed(42)
@@ -32,22 +33,24 @@ class EmsenbleAdaBoost:
         cell_line = '-'.join(data.split('-')[1:3]).split('.')[0]
         data_source = '-'.join(data.split('-')[1:]).split('.')[0]
         for fold in range(5):
-            self.sample_weights = np.ones(len(dataset)) / len(dataset)
-            data = dataset[dataset['fold'] != i]
+            data = dataset[dataset['fold'] != fold]
             features = data.iloc[:, 2:26].values
             target = data.iloc[:, -2].values
             features = torch.tensor(features, dtype=torch.float32)
             target = torch.tensor(target, dtype=torch.float32)
+            sample_weights = np.ones(len(target))
 
             # aggravated predictions
             agg_predictions = np.zeros(len(target))
 
             # each round creates performs the boost on a new set of models
             for i in range(self.n_rounds):
+                models = []
+                alphas = []
                 # create a new set of models
                 for base_learner in self.base_learners:
-                    save_path = pjoin('models', 'trained-models', 'ensemble', 'adaboost', f'{base_learner}-{data_source}-fold-{fold}-round-{i}')
-                    if base_learner == 'mlp_weighted':
+                    save_path = pjoin('models', 'trained-models', 'ensemble', 'adaboost', f'{base_learner}-{data_source}-fold-{fold+1}-round-{i+1}')
+                    if base_learner in self.dl_models:
                         model = self.base_learners[base_learner](save_path=save_path)
                     else:
                         model = self.base_learners[base_learner]()
@@ -59,7 +62,15 @@ class EmsenbleAdaBoost:
                         else:
                             print(f"Training {base_learner}")
                             target = target.view(-1, 1)
-                            model.fit(features, target, sample_weight=self.sample_weights)
+                            # sample weights need to be applied to all the features
+                            sample_weights = torch.tensor(sample_weights, dtype=torch.float32).view(-1, 1)
+                            feature_X = {
+                                'x': features,
+                                'sample_weight': sample_weights
+                            }
+                            sample_weights = sample_weights.view(-1)
+                            sample_weights = sample_weights.numpy().flatten()
+                            model.fit(feature_X, target)
                             target = target.view(-1)
                     else:
                         if isfile(f'{save_path}.pkl'):
@@ -67,19 +78,88 @@ class EmsenbleAdaBoost:
                                 model = pickle.load(f)
                         else:
                             print(f"Training {base_learner}")
-                            model.fit(features, target, sample_weight=self.sample_weights)
+                            model.fit(features, target, sample_weight=sample_weights)
                             with open(f'{save_path}.pkl', 'wb') as f:
                                 pickle.dump(model, f)
 
-                    self.models.append(model)
                     # make predictions
-                    predictions = model.predict(features)
-                    # calculate the error using mean squared error
-                    err = np.sum(self.sample_weights * (predictions - target)) / np.sum(self.sample_weights)
-                    # calculate the model weight alpha
-                    alpha = self.learning_rate * 0.5 * np.log((1 - err) / (err + 1e-10))
+                    predictions = model.predict(features).flatten()
+                    predictions = np.array(predictions)
+                    target_np = np.array(target).flatten()
+                    # calculate the error using pearson correlation
+                    # loss constrained within [0, 1]
+                    print(f"Round {i+1} {base_learner} {pearsonr(predictions, target_np)[0]}")
+                    # calculate the weight of the model
+                    alpha = self.learning_rate * pearsonr(predictions, target_np)[0]
+                    errs = np.abs(predictions - target_np)
+                    # standardize the errors
+                    errs = (errs - np.mean(errs)) / np.std(errs)
+                    # update the sample weights using absolute error
+                    sample_weights = sample_weights * np.exp(alpha * errs)
+                    # clip the sample weights to be within [0.01, 10]
+                    sample_weights = np.clip(sample_weights, 0.01, 10)
+                    # make sure sample weights have mean 1
+                    sample_weights = sample_weights * len(sample_weights) / np.sum(sample_weights) 
                     # update the aggregated predictions
                     agg_predictions += alpha * predictions
-                    # update the sample weights
-                
-                
+
+                    # add the model to the list
+                    models.append(model)
+                    alphas.append(alpha)
+
+            # normalize the weights
+            alphas = np.array(alphas)
+            alphas = alphas / np.sum(alphas)
+            self.models.append(models)
+            self.alphas.append(alphas)
+
+
+        return self.models, self.alphas
+    
+    def test(self, data: str):
+        """Perform the prediction using the trained models
+        
+        Args:
+            data (str): the data to perform the prediction on
+        """
+        dataset = pd.read_csv(pjoin('models', 'data', 'ensemble', data))
+        performances_pearson = collections.defaultdict(list)
+        performances_spearman = collections.defaultdict(list)
+        data_source = '-'.join(data.split('-')[1:]).split('.')[0]
+
+        for i in range(5):
+            alphas = self.alphas[i].flatten()
+            models = self.models[i]
+
+            data = dataset[dataset['fold'] == i]
+            features = data.iloc[:, 2:26].values
+            target = data.iloc[:, -2].values
+            features = torch.tensor(features, dtype=torch.float32)
+            target = torch.tensor(target, dtype=torch.float32)
+
+            # run the performance using base learners for comparison
+            for base_learner in self.base_learners:
+                save_path = pjoin('models', 'trained-models', 'ensemble', 'weighted-mean', f'{base_learner}-{data_source}-fold-{i+1}')
+                if base_learner in self.dl_models:
+                    model = self.base_learners[base_learner](save_path=save_path)
+                    model.initialize()
+                    model.load_params(f_params=f'{save_path}.pt')
+                else:
+                    with open(f'{save_path}.pkl', 'rb') as f:
+                        model = pickle.load(f)
+                predictions = model.predict(features).flatten()
+                performances_pearson[base_learner].append(pearsonr(predictions, target)[0])
+                performances_spearman[base_learner].append(spearmanr(predictions, target)[0])
+
+            # aggregated predictions
+            agg_predictions = np.zeros(len(target))
+
+            for model, alpha in zip(models, alphas):
+                predictions = model.predict(features).flatten()
+                agg_predictions += alpha * predictions
+
+            # calculate the performance
+            performances_pearson['ada'].append(pearsonr(agg_predictions, target)[0])
+            performances_spearman['ada'].append(spearmanr(agg_predictions, target)[0])
+        
+        return performances_pearson, performances_spearman
