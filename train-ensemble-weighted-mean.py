b815a2379e8629231c14db9e93b6cfa93acbf0d6
PHL <s1948149@ed.ac.uk>
2024-08-28 20:50:32 +0100

    finished tuning bagging

--- train-ensemble-weighted-mean.py
+++ train-ensemble-weighted-mean.py
@@ -1,6 +1,11 @@
 from models.ensemble_weighted_mean import EnsembleWeightedMean
+from glob import glob
+from os.path import join as pjoin, basename
 
 model = EnsembleWeightedMean(optimization=False)
 
-for data in ['ensemble-pd-adv-pe2.csv', 'ensemble-pd-k562-pe2.csv', 'ensemble-pd-k562mlh1dn-pe2.csv',  'ensemble-pd-hek293t-pe2.csv']:
-    model.fit(data)
+# for data in ['ensemble-pd-adv-pe2.csv', 'ensemble-pd-k562-pe2.csv', 'ensemble-pd-k562mlh1dn-pe2.csv',  'ensemble-pd-hek293t-pe2.csv']:
+#     model.fit(data)
+
+for data in glob(pjoin('models', 'data', 'ensemble', '*small*.csv')):
+    model.fit(basename(data), fine_tune=True)
